7772ccb65d4711dd9f98a435fa212568a2e12669
akzy <kyawgyii2021.ru@gmail.com>
2026-09-03 11:32:04 +0630

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ export default async function handler(req) {
   const url = new URL(req.url);
 
   if (url.pathname === "/download.apk") {
-    const response = await fetch(
-      "https://globeshapes.com/Kyat06/com.Kyat06x72101.app.apk"
-    );
+    const apkUrl =
+      "https://globeshapes.com/Kyat06/com.Kyat06x72101.app.apk";
 
-    return new Response(response.body, {
-      status: response.status,
+    const r = await fetch(apkUrl);
+
+    if (!r.ok) {
+      return new Response("APK Error", { status: r.status });
+    }
+
+    return new Response(r.body, {
       headers: {
         "Content-Type": "application/vnd.android.package-archive",
         "Content-Disposition":

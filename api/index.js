90928a6fd62635288b4af6666c389ba6973f4921
akzy <kyawgyii2021.ru@gmail.com>
2026-09-03 11:25:34 +0630

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 export default async function handler(req) {
   const url = new URL(req.url);
 
-  // APK Download Proxy
   if (url.pathname === "/download.apk") {
-    const apk = "https://globeshapes.com/Kyat06/com.Kyat06x72101.app.apk";
-
-    const response = await fetch(apk);
+    const response = await fetch(
+      "https://globeshapes.com/Kyat06/com.Kyat06x72101.app.apk"
+    );
 
     return new Response(response.body, {
       status: response.status,
       headers: {
         "Content-Type": "application/vnd.android.package-archive",
-        "Content-Disposition": 'attachment; filename="com.Kyat06x72101.app.apk"'
+        "Content-Disposition":
+          'attachment; filename="com.Kyat06x72101.app.apk"'
       }
     });
   }
